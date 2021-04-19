d63e6dceea0017f2bd7ba201936b9d5b4385ab7b
Simon <simon.altamirano@sjsu.edu>
2021-04-19 14:15:48 -0700

    Image rendering

--- init.py
+++ init.py
@@ -1,19 +1,16 @@
-from flask import Flask
-from flask import render_template, url_for, redirect, request
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('home')
+picFolder = os.path.join('static', 'images')
+app.config['UPLOAD_FOLDER'] = picFolder
+
 @app.route('/')
 def home():
-   return render_template('home.html')
+   pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'gamesLOGO.png')
+   return render_template('home.html', user_image = pic1)
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-   app.run(debug=true)
-=======
    app.run()
 
-def signup():
-   return render_template('signup_login.html')
->>>>>>> 5b082e6b55794da2e1d17919729d9bc9607468ef
