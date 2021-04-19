af18b84f28276db78ffcdc771135de78e49de9df
Simon <simon.altamirano@sjsu.edu>
2021-04-19 13:51:04 -0700

    New HTML

--- init.py
+++ init.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
+@app.route('home')
 @app.route('/')
 def home():
    return render_template('home.html')
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=true)
