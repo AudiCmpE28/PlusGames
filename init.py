1018378c640bc494afd6ca79f8f11b4fd3e5f8f8
Simon <simon.altamirano@sjsu.edu>
2021-04-19 16:09:39 -0700

    Webpage Home

--- init.py
+++ init.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
 import os
 
+#fetches the image
 LOGO_PICTURE = os.path.join('static', 'images')
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = LOGO_PICTURE
+app.config['UPLOAD_FOLDER'] = LOGO_PICTURE #obtains the folder where image is
 
 @app.route('/')
 @app.route('/home')
 def home():
-   logo_image = os.path.join(app.config['UPLOAD_FOLDER'], 'gamesLOGO.jpg')
+   logo_image = os.path.join(app.config['UPLOAD_FOLDER'], 'gamesLOGO.jpg') #pick image
    return render_template('home.html', user_image = logo_image)
 
-# if __name__ == '__main__':
-#    app.run()
 
+
+if __name__ == '__main__':
+   app.run()
