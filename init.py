abd27b273c1c610ab268d4f7f80a11a3616ddf30
Simon <simon.altamirano@sjsu.edu>
2021-04-21 01:17:05 -0700

    Nada

--- init.py
+++ init.py
@@ -8,27 +8,18 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
    
+   
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if(request.method == 'GET'):
-#         return render_template('signup_login.html')
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('home'))
 
-#     email = request.form['email']
-#     passwd = request.form['passwd']
-#     data = getUsersData()
-#     for key in data:
-#         if(email == data[key]['email'] and passwd == data[key]['pass']):
-#             user = User()
-#             user.id = email
-#             flask_login.login_user(user, remember=True)
-#             next = request.args.get('next')
-#             if(next):
-#                 return redirect(next)
-#             else:
-#                 return redirect(url_for('home'))
-
-#     abort(401) # if inside the for, it can't find valid username and password  
+    # show the form, it wasn't submitted
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
