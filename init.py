0e94c46f4b1e9354871e1f3f187a25ae758b6371
Simon <simon.altamirano@sjsu.edu>
2021-04-20 17:04:49 -0700

    Complete overhaul of Homepage

--- init.py
+++ init.py
@@ -1,21 +1,13 @@
 from flask import Flask, render_template
 import os
 
-#fetches the image
-LOGO_PICTURE = os.path.join('static', 'images')
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = LOGO_PICTURE #obtains the folder where image is
 
 @app.route('/')
 @app.route('/home')
 def home():
-   logo_image = os.path.join(app.config['UPLOAD_FOLDER'], 'gamesLOGO.jpg') #pick image
-   logo_gif = os.path.join(app.config['UPLOAD_FOLDER'], 'logoCool.gif')
-
-   return render_template('home.html', user_image = logo_image)
-
-
+   return render_template('home.html')
+   
 
 if __name__ == '__main__':
    app.run()
