493429f29f41ae5e44ac721fb383e15b2b88b9b3
jasdipsekhon <jasdipsekhon@gmail.com>
2021-04-21 23:47:25 -0700

    fixed manage table

--- sql.sql
+++ sql.sql
@@ -31,7 +31,7 @@ CREATE TABLE Game(
 
 
 CREATE TABLE Released_on (
-game_id VARCHAR(25) not null,
+game_id 	  VARCHAR(25) not null,
 platform_name VARCHAR(25) not null,
 primary key (game_id,platform_name),
 foreign key (game_id) references Game(game_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -52,8 +52,9 @@ foreign key (unique_id) references Users(unique_id)
 on update cascade on delete cascade
 );
 
+
 CREATE TABLE Members (
-unique_id	integer(16)not null,
+unique_id		integer(16)not null,
 mem_username	varchar(16)not null,
 mem_password	varchar(255) default null, -- insert hashed passwords by using a select statment and MD5('password')
 primary key		(unique_id, mem_username),  -- temporary, might need to add email column for these tables
@@ -62,18 +63,17 @@ on update cascade on delete cascade,
 unique (mem_username)
 );
 
-
 CREATE TABLE request_game (
-mem_username	varchar(16)not null,
-game_id       varchar(25),
-req_text	varchar(50),
+mem_username	varchar(16) not null,
+game_id       	varchar(25),
+req_text		varchar(50),
 foreign key (mem_username) references Members(mem_username) ON UPDATE CASCADE ON DELETE CASCADE,
 foreign key (game_id) references Game(game_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE comment_on (
 mem_username	varchar(16)not null,
-game_id		varchar(25)not null,
+game_id			varchar(25)not null,
 c_date	date,
 c_time 	timestamp,
 comment_text varchar(250),
@@ -85,7 +85,7 @@ foreign key (game_id) references Game(game_id) on delete CASCADE
 CREATE TABLE review_on (
 mem_username	varchar(16)not null,
 game_id		varchar(10)not null,
-rv_date	date,
+rv_date		date,
 rv_time 	timestamp,
 review_text varchar(250),
 primary key (mem_username, game_id), 
@@ -95,7 +95,7 @@ foreign key (game_id) references Game(game_id) on delete CASCADE
 
 CREATE TABLE report_on (
 mem_username	varchar(16)not null,
-game_id		varchar(10)not null,
+game_id			varchar(10)not null,
 rp_date	date,
 rp_time 	timestamp,
 report_text varchar(250),
@@ -106,42 +106,39 @@ foreign key (game_id) references Game(game_id) on delete CASCADE
 
 CREATE TABLE bookmarked (
 mem_username	varchar(16) not null,
-game_id		varchar(10) not null,
+game_id			varchar(10) not null,
 primary key (mem_username, game_id), 
 foreign key (mem_username) references Members(mem_username) on delete CASCADE,
 foreign key (game_id) references Game(game_id) on delete CASCADE
 );
 
 CREATE TABLE Administrator (
-unique_id	integer(16) not null,
+unique_id		integer(16) not null,
 admin_username	varchar(16) not null,
 admin_password	varchar(255) default null,
 primary key (unique_id, admin_username),
 foreign key (unique_id) references Users(unique_id)
-on update cascade on delete cascade
+on update cascade on delete cascade,
+unique (admin_username)
 );
 -- Create test cases to verify insertion/deletion works as expected
---  new (Pls check to see if its correct)
 CREATE TABLE Profile (
 user_id integer(16) not null,
-name varchar(16),
+name 	varchar(16),
 primary key (user_id)
-
 ); 
 
---  new (Pls check to see if its correct)
 CREATE TABLE interact_with (
 mem_username	varchar(16),
-user_id       integer(16),
+user_id       	integer(16),
 primary key (mem_username, user_id),
 foreign key (mem_username) references Members(mem_username) ON DELETE CASCADE,
 foreign key (user_id) references Profile(user_id) ON DELETE CASCADE
 );
 
---  new (Pls check to see if its correct)
 CREATE TABLE manage (
-admin_username	varchar(16)  not null,
-user_id       integer(16) not null,
+admin_username	varchar(16) not null,
+user_id       	integer(16) not null,
 primary key (admin_username, user_id),
 foreign key (admin_username) references Administrator(admin_username) ON DELETE CASCADE,
 foreign key (user_id) references Profile(user_id) ON DELETE CASCADE
