9e98b0cbc5f24ef0db14579bbed0736dd3254683
christinemnguyen <christine.ng17@gmail.com>
2021-04-25 01:00:04 -0700

    db request in example

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import os
+import os, sys, request
 
 app = Flask(__name__)
 
@@ -26,7 +26,10 @@ def request():
 
 @app.route('/example', methods=['GET', 'POST'])
 def example():
-   return render_template('example.html')
+   result = int(request.form['result'])
+   cursor = connection.cursor()
+   cursor.execute("get database testing result", result)
+   return render_template('example.html', result = result)
 
 
 if __name__ == '__main__':
