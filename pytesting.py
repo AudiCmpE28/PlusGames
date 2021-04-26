7aabb3e975eea0802ad49415fb15a209c64ced3a
dahmedk1999 <danish.khan@sjsu.edu>
2021-04-25 20:00:17 -0700

    remember to comment/uncomment

--- pytesting.py
+++ pytesting.py
@@ -16,7 +16,7 @@ INSERT Company VALUES
 ('Microsoft'),('Bethesda'),('Arkane'),('Epic'),('Steam'),('Frontier'),('Ubisoft'),('Mojang');
 """
 #Since we can only have unique companies, run only once
-#execute_query(connection, companyinsert)
+execute_query(connection, companyinsert)
 
 #-----------------------------------------------------
 #Retrieving data and tabulating it in python
@@ -61,13 +61,13 @@ for result in qresult:
 #     execute_query(connection,userquery)
 
 ## suppose these random strings are user inputs from the website
-# for i in range(0,10):
-#     uniqueid= random.randint(1,100000)
-#     username= randomstring(16)
-#     email=randomstring(5)+'@'+randomstring(5)+'.com'
-#     password= randomstring(25)
-#     print('\nUnique_id:'+str(uniqueid)+'\nUsername:'+username+'\nEmail:'+email+'\nPassword:'+password)
-#     addmembers(connection, uniqueid,username,email,password)
+for i in range(0,10):
+    uniqueid= random.randint(1,100000)
+    username= randomstring(16)
+    email=randomstring(5)+'@'+randomstring(5)+'.com'
+    password= randomstring(25)
+    print('\nUnique_id:'+str(uniqueid)+'\nUsername:'+username+'\nEmail:'+email+'\nPassword:'+password)
+    addmembers(connection, uniqueid,username,email,password)
 
 
 # Tabulate the query results!
