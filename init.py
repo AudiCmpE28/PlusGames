7390830a46fe914ca5c3cad20c5045b9232a4745
christinemnguyen <christine.ng17@gmail.com>
2021-04-25 21:57:09 -0700

    fix request

--- init.py
+++ init.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 
 import random, string
 import os, sys
-from flask_msqldb import MySQL
-from pyconnector.py import *
+# from flask_msqldb import MySQL
+# from pyconnector.py import * #pyconnector not a package
 
 
 app = Flask(__name__)
@@ -19,14 +19,14 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method == "POST": ##gets info from form
-      userDetails = request.form
-      email= userDetails['email']
-      password = userDetails['password']
-      cur = mysql.connection.cursor() #open cursor
-      cursor.execute("Insert INTO users(email, password) VALUES(%s, %s)", (username, email))
-      cur.close()
-   else:
+   # if request.method == "POST": ##gets info from form
+   #    userDetails = request.form
+   #    email= userDetails['email']
+   #    password = userDetails['password']
+   #    cur = mysql.connection.cursor() #open cursor
+   #    cursor.execute("Insert INTO users(email, password) VALUES(%s, %s)", (username, email))
+   #    cur.close()
+   # else:
       return render_template('login.html')
 
 
@@ -49,17 +49,17 @@ def signup():
    #       exit
    return render_template('signup.html')
 
-@app.route('/request', methods=['GET', 'POST'])
-def request():
-   return render_template('request.html')
+@app.route('/request_page', methods=['GET', 'POST'])
+def request_page():
+   return render_template('request_page.html')
 
 
 @app.route('/example', methods=['GET', 'POST'])
 def example():
-   result = (request.form['result'])
-   cursor = connection.cursor()
-   cursor.execute("get database testing result", result)
-   return render_template('example.html', result = result)
+   # result = (request.form['result'])
+   # cursor = connection.cursor()
+   # cursor.execute("get database testing result", result)
+   return render_template('example.html')
 
 
 @app.route('/game_page', methods=['GET', 'POST'])
