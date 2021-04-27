e5461841776820bd26ccb0bd1f746498ecfa17f1
jasdipsekhon <jasdipsekhon@gmail.com>
2021-04-26 21:10:05 -0700

    added addbookmark()

--- pyconnector.py
+++ pyconnector.py
@@ -183,3 +183,6 @@ def gamecomments(connection, game_id):
     game_comments = "select mem_username, c_date, c_time, comment_text from comment_on join Game on comment_on.game_id = Game.game_id where game_id={} order by c_time desc".format(game_id)
     return returncolumns(connection,game_comments)
 
+def addbookmark(connection, mem_username, game_id):
+        insertq="insert into bookmarked (mem_username, game_id) values ('{}', '{}');".format(mem_username, game_id)
+        execute_query(connection, insertq)
