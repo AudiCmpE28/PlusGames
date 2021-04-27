011b0093daf0c8c185bde3b966ea1583164175d9
Simon <simon.altamirano@sjsu.edu>
2021-04-26 21:43:06 -0700

    Nada

--- init.py
+++ init.py
@@ -110,7 +110,8 @@ def profile():
       print(rating)
       print(release_Date)
       print(price)
-      ##
+      
+
       try:
          sortbyalphabetical(mysql.connection)
          return render_template('profile.html')
